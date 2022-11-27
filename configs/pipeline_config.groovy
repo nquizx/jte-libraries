e362cf1f5009c17ba31f47f53ef75d55a2725dcd
nquizx <nquizx@gmail.com>
2022-11-27 16:18:56 +0700

    Test

--- configs/pipeline_config.groovy
+++ configs/pipeline_config.groovy
@@ -2,8 +2,23 @@ jte {
   allow_scm_jenkinsfile = false
 }
 
+// template_methods {
+// }
+
 libraries {
   css
   sast
   sca
+}
+
+stages {
+  @override build {}
+  @merge test {}
+  security_scan {
+    css_scan
+    sast_scan
+    sca_scan
+  }
+  @merge publish_artifact {}
+  @override deploy {}
 }
