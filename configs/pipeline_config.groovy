1ac487ae5ce3588dcecd23d67facde011ac023f4
nquizx <nquizx@gmail.com>
2022-11-27 16:25:19 +0700

    Test 2

--- configs/pipeline_config.groovy
+++ configs/pipeline_config.groovy
@@ -12,13 +12,9 @@ libraries {
 }
 
 stages {
-  @override build {}
-  @merge test {}
   security_scan {
     css_scan
     sast_scan
     sca_scan
   }
-  @merge publish_artifact {}
-  @override deploy {}
 }
